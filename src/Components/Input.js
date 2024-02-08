b7d83a0bf5d631b48630423058d5db5b89d5c128
Theophilus Frimpong <theophilusfrimpong17@gmail.com>
2024-02-08 12:24:55 +0000

    finished working on he signup and signin pages

--- src/Components/Input.js
+++ src/Components/Input.js
@@ -1,27 +1,51 @@
-"use client";
-import React from "react";
-// import { useRef } from "react";
+import React, { useState } from "react";
 
-export default function Input({ placeholder, icon }) {
-  // const containerRef = useRef(null);
-
-  // const handleClick = () => {
-  //   if (containerRef.current) {
-  //     containerRef.current.focus();
-  //   }
-  // };
+const FormInput = ({
+  type,
+  placeholder,
+  required,
+  onChange,
+  value,
+  name,
+  error,
+  resetError,
+  leftIcon,
+  rightIcon,
+  isValid,
+}) => {
   return (
-    <div
-      // ref={containerRef}
-      // tabIndex={0}
-      // onClick={handleClick}
-      className="w-72 sm:w-[400px] flex justify-center items-center h-12 focus:outline-none  focus:border-blue-500 shadow-sm border-2 rounded-xl p-2"
-    >
-      {icon}
-      <input
-        className=" w-full h-full ml-1 focus:outline-none"
-        placeholder={placeholder}
-      />
+    <div className="w-full md:w-3/4">
+      <div className="relative w-full">
+        {leftIcon && (
+          <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+            {leftIcon}
+          </div>
+        )}
+        <input
+          onFocus={resetError}
+          type={type}
+          name={name}
+          placeholder={placeholder}
+          onChange={onChange}
+          value={value}
+          required={required}
+          className={`bg-gray-50 border ${
+            error ? "border-red-600" : "border-gray-300"
+          }  text-gray-900 text-md rounded-lg focus:ring-2 focus:ring-blue-200 focus:border-blue-500 block
+           w-full ${leftIcon && "pl-10"} p-2.5 outline-none `}
+        />
+        {rightIcon && (
+          <div className="absolute inset-y-0 right-0 flex items-center pr-3 cursor-pointer">
+            {rightIcon}
+          </div>
+        )}
+      </div>
+      {!isValid?.isValid && error === null && (
+        <span className="mt-2 text-sm text-red-600">{isValid?.msg}</span>
+      )}
+      {error && <span className="mt-2 text-sm text-red-600">{error}</span>}
     </div>
   );
-}
+};
+
+export default FormInput;
--- src/app/Authentication/signIn/page.js
+++ src/app/Authentication/signIn/page.js
@@ -1,38 +1,113 @@
 "use client";
 import Image from "next/image";
-import React from "react";
-import Input from "@/Components/Input";
+import React, { useState } from "react";
+import FormInput from "@/Components/Input";
+import { emailValidation, passwordValidation } from "@/Validation";
 import Button from "@/Components/Button";
 import IconBtn from "@/Components/IconBtn";
 import { MdEmail } from "react-icons/md";
 import { CiLock } from "react-icons/ci";
 import { FcGoogle } from "react-icons/fc";
 import { FaApple } from "react-icons/fa";
+import { FaEye } from "react-icons/fa";
+import { FaEyeSlash } from "react-icons/fa";
 
 export default function SignIn() {
+  // const [email, setEmail] = useState("email");
+  // const [show, setShow] = useState(false);
+
+  const [isValid, setIsValid] = useState(true);
+  const [error, setError] = useState({
+    email: "",
+    password: "",
+  });
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+  const [show, setShow] = useState(false);
+
+  const handleTogglePassword = () => {
+    setShow((prev) => !prev);
+  };
+
+  const handleInputChange = (e) => {
+    let errorMessage = "";
+    const { value, name } = e.target;
+
+    //Validate email field
+    if (name === "email") {
+      const validation = emailValidation(value);
+      setIsValid(validation.isValid);
+      errorMessage = validation.msg;
+    }
+
+    //Validate password
+    if (name === "password") {
+      const validation = passwordValidation(value);
+      setIsValid(validation.isValid);
+      errorMessage = validation.msg;
+    }
+
+    setError({ ...error, [name]: errorMessage });
+    setForm({ ...form, [name]: value });
+    // console.log(error.email);
+  };
+
+  // const handleError = (e) => {};
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(form);
+    if (isValid === false) {
+      console.log("No entry");
+    } else {
+      console.log("Full entry");
+    }
+  };
+
   return (
     <main className="flex h-screen flex-col justify-center items-center">
       <div className="h-1/3 w-full flex justify-center items-end">
         <Image src={"/User-avatar.png"} alt="" height={150} width={150} />
       </div>
-      <div className="h-2/6  w-full">
-        <form className="flex h-full flex-col justify-evenly  items-center ">
-          <label className=" h-1/2 flex justify-end items-end">
-            <Input
-              placeholder={"Enter your email"}
-              icon={<MdEmail size={20} className="text-gray-300" />}
-            />
-          </label>
-          <label>
-            <Input
-              placeholder={"Enter your password"}
-              icon={<CiLock size={25} className="text-gray-300" />}
-            />
-          </label>
+      <div className="w-full flex justify-center h-72">
+        <form
+          onSubmit={handleSubmit}
+          className="flex h-full w-4/5 sm:w-1/2 flex-col justify-evenly  items-center"
+        >
+          <FormInput
+            placeholder="email@codewave.com"
+            type="email"
+            name="email"
+            onChange={handleInputChange}
+            error={error.email}
+            isValid={isValid}
+            required={true}
+            leftIcon={<MdEmail color={"#666"} />}
+          />
+          <FormInput
+            placeholder="Password"
+            type={show ? "text" : "password"}
+            required={true}
+            name="password"
+            error={error.password}
+            onChange={handleInputChange}
+            leftIcon={<CiLock color={"#666"} />}
+            rightIcon={
+              show ? (
+                <FaEye onClick={handleTogglePassword} />
+              ) : (
+                <FaEyeSlash onClick={handleTogglePassword} />
+              )
+            }
+          />
+          <div className="fle w-full justify-center items-center">
+            <Button string="Sign In" />
+          </div>
         </form>
       </div>
-      <div className="w-full h-64  flex flex-col justify-evenly items-center">
-        <Button string="Sign In" />
+      <div className="w-full h-44  flex flex-col justify-evenly items-center">
         Or Sign In With
         <div className="flex flex-row justify-evenly lg:justify-center w-full md:w-1/2 ">
           <IconBtn string="Google" icon={<FcGoogle size={30} />} />
